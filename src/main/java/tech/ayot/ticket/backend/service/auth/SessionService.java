68e35d84da77ad1c4c570cb383b9510cf9346669
Mohammad Sadeghi <mohammad.s@smartech.ir>
2023-12-16 15:59:13 +0330

    feat: add integration tests for register

--- src/main/java/tech/ayot/ticket/backend/service/auth/SessionService.java
+++ src/main/java/tech/ayot/ticket/backend/service/auth/SessionService.java
@@ -28,9 +28,9 @@ import java.util.Map;
 @Transactional(readOnly = true)
 public class SessionService<S extends Session> implements UserDetailsService {
 
-    private static final String SPRING_SECURITY_CONTEXT_TOKEN = "SPRING_SECURITY_CONTEXT";
+    public static final String SPRING_SECURITY_CONTEXT_TOKEN = "SPRING_SECURITY_CONTEXT";
 
-    private static final String USERNAME_TOKEN = JdbcIndexedSessionRepository.PRINCIPAL_NAME_INDEX_NAME;
+    public static final String USERNAME_TOKEN = JdbcIndexedSessionRepository.PRINCIPAL_NAME_INDEX_NAME;
 
     private final FindByIndexNameSessionRepository<S> sessionRepository;
 
--- src/test/java/tech/ayot/ticket/backend/integration/auth/AuthenticationServiceIntegrationTest.java
+++ src/test/java/tech/ayot/ticket/backend/integration/auth/AuthenticationServiceIntegrationTest.java
@@ -1,5 +1,6 @@
 package tech.ayot.ticket.backend.integration.auth;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpMethod;
@@ -14,10 +15,12 @@ import tech.ayot.ticket.backend.model.user.User;
 import tech.ayot.ticket.backend.repository.user.UserRepository;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static tech.ayot.ticket.backend.service.auth.SessionService.USERNAME_TOKEN;
 
 public class AuthenticationServiceIntegrationTest extends BaseIntegrationTest {
 
@@ -41,6 +44,26 @@ public class AuthenticationServiceIntegrationTest extends BaseIntegrationTest {
     }
 
 
+    @AfterEach
+    public void cleanUp() {
+        // Get all users but admin user
+        List<User> users = userRepository.findAll();
+        users = users.stream().filter(user -> !ADMIN_USER.equals(user.getUsername())).toList();
+
+        // Delete sessions
+        users.forEach(user -> {
+            Map<String, ? extends Session> sessionsMap = sessionRepository.findByIndexNameAndIndexValue(
+                USERNAME_TOKEN,
+                user.getUsername()
+            );
+            sessionsMap.values().forEach(session -> sessionRepository.deleteById(session.getId()));
+        });
+
+        // Delete users
+        userRepository.deleteAll(users);
+    }
+
+
     @Test
     public void loginShouldReturn200IfUsernameAndPasswordAreValid() throws Exception {
         String username = testName + "_username";
@@ -50,39 +73,34 @@ public class AuthenticationServiceIntegrationTest extends BaseIntegrationTest {
         user.setPassword(passwordEncoder.encode(password));
         userRepository.save(user);
 
-        try {
-            Instant beforeLogin = Instant.now();
-            LoginRequest loginRequest = new LoginRequest(
-                username,
-                password
-            );
-            sendRequest(
-                HttpMethod.POST,
-                "/api/auth/login",
-                MediaType.APPLICATION_JSON,
-                loginRequest,
-                status().isOk()
-            );
+        Instant beforeLogin = Instant.now();
+        LoginRequest loginRequest = new LoginRequest(
+            username,
+            password
+        );
+        sendRequest(
+            HttpMethod.POST,
+            "/api/auth/login",
+            MediaType.APPLICATION_JSON,
+            loginRequest,
+            status().isOk()
+        );
 
-            Map<String, ? extends Session> sessiosMap = sessionRepository.findByIndexNameAndIndexValue(
-                JdbcIndexedSessionRepository.PRINCIPAL_NAME_INDEX_NAME,
-                user.getUsername()
-            );
-            Optional<? extends Session> optionalSession = sessiosMap.values().stream()
-                .filter(session -> session.getCreationTime().isAfter(beforeLogin))
-                .findFirst();
-            Assertions.assertTrue(optionalSession.isPresent());
-            sessionRepository.deleteById(optionalSession.get().getId());
-        } finally {
-            userRepository.delete(user);
-        }
+        Map<String, ? extends Session> sessiosMap = sessionRepository.findByIndexNameAndIndexValue(
+            JdbcIndexedSessionRepository.PRINCIPAL_NAME_INDEX_NAME,
+            user.getUsername()
+        );
+        Optional<? extends Session> optionalSession = sessiosMap.values().stream()
+            .filter(session -> session.getCreationTime().isAfter(beforeLogin))
+            .findFirst();
+        Assertions.assertTrue(optionalSession.isPresent());
     }
 
     @Test
-    public void loginShouldReturn400IfPasswordDoesNotMatchPattern() throws Exception {
+    public void loginShouldReturn400IfUsernameDoesNotMatchPattern() throws Exception {
         LoginRequest loginRequest = new LoginRequest(
-            "username",
-            "password"
+            "username!",
+            "P@33word"
         );
         sendRequest(
             HttpMethod.POST,
@@ -94,10 +112,10 @@ public class AuthenticationServiceIntegrationTest extends BaseIntegrationTest {
     }
 
     @Test
-    public void loginShouldReturn400IfUsernameDoesNotMatchPattern() throws Exception {
+    public void loginShouldReturn400IfPasswordDoesNotMatchPattern() throws Exception {
         LoginRequest loginRequest = new LoginRequest(
-            "username!",
-            "P@33word"
+            "username",
+            "password"
         );
         sendRequest(
             HttpMethod.POST,
@@ -117,21 +135,68 @@ public class AuthenticationServiceIntegrationTest extends BaseIntegrationTest {
         user.setPassword(passwordEncoder.encode(password));
         userRepository.save(user);
 
-        try {
-            Instant beforeLogin = Instant.now();
-            LoginRequest loginRequest = new LoginRequest(
-                username,
-                password + "X"
-            );
-            sendRequest(
-                HttpMethod.POST,
-                "/api/auth/login",
-                MediaType.APPLICATION_JSON,
-                loginRequest,
-                status().isUnauthorized()
-            );
-        } finally {
-            userRepository.delete(user);
-        }
+        Instant beforeLogin = Instant.now();
+        LoginRequest loginRequest = new LoginRequest(
+            username,
+            password + "X"
+        );
+        sendRequest(
+            HttpMethod.POST,
+            "/api/auth/login",
+            MediaType.APPLICATION_JSON,
+            loginRequest,
+            status().isUnauthorized()
+        );
+    }
+
+    @Test
+    public void registerShouldReturn200AndCreateUser() throws Exception {
+        String username = testName + "username";
+        String password = testName + "P@33word";
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        sendRequest(
+            HttpMethod.POST,
+            "/api/auth/register",
+            MediaType.APPLICATION_JSON,
+            user,
+            status().isOk()
+        );
+
+        User createdUser = userRepository.findUserByUsername(username);
+        Assertions.assertNotNull(createdUser);
+    }
+
+    @Test
+    public void registerShouldReturn400AndIfUsernameDoesNotMatchPattern() throws Exception {
+        String username = testName + "username!";
+        String password = testName + "P@33word";
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        sendRequest(
+            HttpMethod.POST,
+            "/api/auth/register",
+            MediaType.APPLICATION_JSON,
+            user,
+            status().isBadRequest()
+        );
+    }
+
+    @Test
+    public void registerShouldReturn400AndIfPasswordDoesNotMatchPattern() throws Exception {
+        String username = testName + "username";
+        String password = testName + "Pa33word";
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        sendRequest(
+            HttpMethod.POST,
+            "/api/auth/register",
+            MediaType.APPLICATION_JSON,
+            user,
+            status().isBadRequest()
+        );
     }
 }
