e8d9d4e32a37996c239b39edfae641bde68bab5d
Mohammad Sadeghi <mohammad.s@smartech.ir>
2024-01-29 14:28:27 +0330

    hotfix: fix tests

--- src/test/java/tech/ayot/ticket/backend/integration/product/ProductServiceIntegrationTest.java
+++ src/test/java/tech/ayot/ticket/backend/integration/product/ProductServiceIntegrationTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import tech.ayot.ticket.backend.MockMvcResponse;
 import tech.ayot.ticket.backend.dto.auth.enumuration.Role;
 import tech.ayot.ticket.backend.dto.product.request.CreateProductRequest;
 import tech.ayot.ticket.backend.dto.product.request.UpdateProductRequest;
