be5c047c7e32b6e8edfff0b2ab068cbbe2d0a2da
Mahdi Abbastabaar <abbastabar.mahdi1381@gmail.com>
2024-01-29 14:40:59 +0330

    feat: add setter and getter

--- src/main/java/tech/ayot/ticket/backend/model/ticket/Message.java
+++ src/main/java/tech/ayot/ticket/backend/model/ticket/Message.java
@@ -18,4 +18,27 @@ public class Message extends BaseModel {
     @ManyToOne
     private User user;
 
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Ticket getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/main/java/tech/ayot/ticket/backend/model/ticket/Ticket.java
+++ src/main/java/tech/ayot/ticket/backend/model/ticket/Ticket.java
@@ -26,4 +26,44 @@ public abstract class Ticket extends BaseModel {
 
     @ManyToOne
     private User user;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
