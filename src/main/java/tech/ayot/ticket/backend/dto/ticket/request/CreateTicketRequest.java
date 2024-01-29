6a10b3ee6d743ef04ee3d860bf00c86bc9447aad
Mahdi Abbastabaar <abbastabar.mahdi1381@gmail.com>
2024-01-29 16:55:03 +0330

    feat: add create ticket api

--- src/main/java/tech/ayot/ticket/backend/dto/ticket/request/CreateTicketRequest.java
+++ src/main/java/tech/ayot/ticket/backend/dto/ticket/request/CreateTicketRequest.java
@@ -1,4 +1,21 @@
 package tech.ayot.ticket.backend.dto.ticket.request;
 
-public class CreateTicketRequest {
+import jakarta.validation.constraints.NotBlank;
+import java.util.Date;
+
+public record CreateTicketRequest (
+    @NotBlank String title,
+    String description,
+    String productName,
+    Date deadline,
+    String type
+
+) {
+    public CreateTicketRequest(String title, String description, String productName, Date deadline, String type) {
+        this.title = title;
+        this.description = description;
+        this.productName = productName;
+        this.deadline = deadline;
+        this.type = type;
+    }
 }
